e0efdc24a14dbbb9fada4d618036a8b16196c4b3
Desy Armariena <desy.armariena@gmail.com>
2018-10-26 21:00:56 +0700

    Add new list to DOM

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// var tes = document.getElementsByTagName('span');
+var input = document.getElementById('input');
+var ul = document.querySelector('ul');
+
+input.addEventListener('keyup', function(e){
+    if(e.keyCode === 13){
+        var teks = input.value;
+        //buat element list untuk ditambah ke dalam unordered list
+        var li = document.createElement('li');
+        li.innerHTML = '<span>X</span> ' + teks;        
+        ul.appendChild(li);
+        input.value = '';
+    }
+});
+
+var lists = document.querySelectorAll('span');
+
+lists.forEach(list => list.addEventListener('click', function(e){
+    // console.log(this.parentElement);
+    this.parentElement.remove();
+}));
